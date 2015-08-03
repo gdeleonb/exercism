07daa0a77b4b09abe44f4a60175d66c93fd925c0
Gustavo De Leon <gus@deleon.org.mx>
2015-08-03 15:25:09 -0700

    Created new constant (translation key) and changed everything to exploit it
    New constant:
     - `KEY` constant is a hash that stores the DNA nucleotides as the index and the RNA nucleotides as the values. This lets me redefine my methods to transcribe both DNA and RNA in a much neater fashion by simply inverting the KEY when needed
    
    Existing method changes (`of_dna` and `of_rna`):
     - Now use `map` instead of `collect!` (Bang form was unnecessary and `map` is simply the more common form of `collect` in Ruby)
     - Now reference new methods (`dna_translate` and `rna_translate`) for the transcription, greatly shortening the length of the `map` block
    
    New methods:
     - `to_rna`
       - Runs the nucleotides through the `KEY` constant to output the transcribed array
       - Uses the `has_key?` method to raise an ArgumentError if a given nucleotide is invalid
     - `to_dna`
       - Same as above, but uses an inverted KEY to create the transcribed array
       - Uses `has_value?` method instead of `has_key?` for the ArgumentError check

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,38 +1,24 @@
 class Complement
   VERSION = 1
 
+  KEY = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+
   def self.of_dna(strand)
-    strand.chars.collect! do |nucleotide|
-      case nucleotide
-      when 'G'
-        'C'
-      when 'C'
-        'G'
-      when 'T'
-        'A'
-      when 'A'
-        'U'
-      else
-        raise ArgumentError
-      end
-    end.join
+    strand.chars.map { |nucleotide| to_rna(nucleotide) }.join
   end
 
   def self.of_rna(strand)
-    strand.chars.collect! do |nucleotide|
-      case nucleotide
-      when 'C'
-        'G'
-      when 'G'
-        'C'
-      when 'U'
-        'A'
-      when 'A'
-        'T'
-      else
-        raise ArgumentError
-      end
-    end.join
+    strand.chars.map { |nucleotide| to_dna(nucleotide) }.join
+  end
+
+  def self.to_rna(nucleotide)
+    raise ArgumentError.new('Invalid DNA sequence') unless KEY.has_key?(nucleotide)
+    KEY[nucleotide]
+  end
+
+  def self.to_dna(nucleotide)
+    raise ArgumentError.new('Invalid RNA sequence') unless KEY.has_value?(nucleotide)
+    KEY.invert[nucleotide]
   end
 
 end
