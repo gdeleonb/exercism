7991e8ffa4f9ae6a0d4652367ce58b42828eade8
Gustavo De Leon <gus@deleon.org.mx>
2015-08-04 11:23:23 -0700

    Creates random names (2 letters and 3 numbers)
    Used two constants to create character banks (one for letters and one for numbers) and then used array generators with the join command to create the name.

---
+++ robot-name/robot_name.rb
@@ -0,0 +1,19 @@
+class Robot
+  LETTER_BANK = [*'A'..'Z']
+  NUMBER_BANK = [*'0'..'9']
+
+  def initialize
+    letters = Array.new(2){ LETTER_BANK.sample }.join
+    numbers = Array.new(3){ NUMBER_BANK.sample }.join
+    name = letters + numbers
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def reset
+    initialize
+  end
+end
