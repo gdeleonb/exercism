9833cc29659ed7dd48171565bf48641333d94fb9
Gustavo De Leon <gus@deleon.org.mx>
2015-08-05 15:23:23 -0700

    Condensed `if` statements into single lines

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,23 +1,13 @@
 class Raindrops
-  output = ''
 
 	def self.convert(number)
-		if (number % 5 != 0) && ( number % 3 != 0) && (number % 7 != 0)
-			output = number.to_s
-		end
-		
-	  if number % 3 == 0
-			output = output.to_s + 'Pling'
-		end
-		
-		if number % 5 == 0
-			output = output.to_s + 'Plang'
-		end
-		
-		if number % 7 == 0
-			output = output.to_s + 'Plong'
-		end
+    output = ''
 
-		output
+    output << 'Pling' if number % 3 == 0
+    output << 'Plang' if number % 5 == 0
+    output << 'Plong' if number % 7 == 0
+    output << number.to_s if output.empty?
+
+    output
 	end
 end
