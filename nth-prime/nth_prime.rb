34023334c9cdc8e70ff3f923a9d70f9d6e7393cd
Gustavo De Leon <gus@deleon.org.mx>
2015-08-12 22:45:13 -0700

    Turned instance variable @test_number local
    Added argument to prime_check method to make variable @test_number local. Might not be strictly necessary, but I just don't like having too many of them

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,23 +3,23 @@ class Prime
   def nth(nth_prime)
     raise ArgumentError unless nth_prime > 0
     @primes = [2]
-    @test_number = 3
+    test_number = 3
 
     while @primes.length != nth_prime
-      prime_check
-      @test_number += 2
+      prime_check(test_number)
+      test_number += 2
     end
     @primes.last
   end
 
-  def prime_check
+  def prime_check(test_number)
     counter = 0
     @primes.each do |prime|
-      if @test_number % prime != 0
+      if test_number % prime != 0
         counter += 1
       end
     end
-    @primes.push(@test_number) if counter == @primes.length
+    @primes.push(test_number) if counter == @primes.length
   end
 
 end
