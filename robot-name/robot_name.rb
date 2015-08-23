7d494be0058c6688f7c967a9c2626b02029c1891
Gustavo De Leon <gus@deleon.org.mx>
2015-08-23 15:59:18 -0700

    Condensed name creation into one line

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -1,11 +1,6 @@
 class Robot
-  LETTER_BANK = [*'A'..'Z']
-  NUMBER_BANK = [*'0'..'9']
-
   def initialize
-    letters = Array.new(2){ LETTER_BANK.sample }.join
-    numbers = Array.new(3){ NUMBER_BANK.sample }.join
-    @name = letters + numbers
+    @name = Array.new(2){ [*'A'..'Z'].sample }.join + Array.new(3){ [*'0'..'9'].sample }.join
   end
 
   def name
