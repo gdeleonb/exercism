bb4ee13696afd17afdc826b21b3a225e47d5bc2d
nazgus <gus@deleon.org.mx>
2017-06-28 22:15:44 -0600

    Fix variable name typo

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -4,6 +4,6 @@ class Raindrops
     output << 'Pling' if number % 3 == 0
     output << 'Plang' if number % 5 == 0
     output << 'Plong' if number % 7 == 0
-    output.empty? ? number.to_s : result
+    output.empty? ? number.to_s : output
   end
 end
