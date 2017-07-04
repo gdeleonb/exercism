cd2ae705222b68fe54534a368df3fe82b8e9d3da
nazgus <gus@deleon.org.mx>
2017-07-03 19:38:21 -0600

    Use two arrays and zipping

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,37 +1,18 @@
 class Fixnum
   def to_roman
-    ms  =  self          / 1000
-    cs  = (self % 1000)  / 100
-    xs  = (self % 100)   / 10
-    is  = (self % 10)
+    raise 'Only works on numbers between 1 and 3000!' if self <= 0 || self > 3000
 
-    roman_num = 'M' * ms
+    ints = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romNums = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+    string = ''
+    remainder = self
 
-    if cs == 9
-      roman_num << 'CM'
-    elsif cs == 4
-      roman_num << 'CD'
-    else
-      roman_num << 'D' * (self % 1000 / 500)
-      roman_num << 'C' * (self %  500 / 100)
+    ints.zip(romNums).each do |int, romNum|
+      quotient = remainder / int
+      string += romNum * quotient
+      remainder = remainder % int
     end
 
-    if xs == 9
-      roman_num << 'XC'
-    elsif xs == 4
-      roman_num << 'XL'
-    else
-      roman_num << 'L' * (self % 100 / 50)
-      roman_num << 'X' * (self %  50 / 10)
-    end
-
-    if is == 9
-      roman_num << 'IX'
-    elsif is == 4
-      roman_num << 'IV'
-    else
-      roman_num << 'V' * (self % 10 / 5)
-      roman_num << 'I' * (self %  5 / 1)
-    end
+    string
   end
 end
