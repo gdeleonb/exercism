cbeda0e77492ba7938112eae02e127520e59bcbf
nazgus <gus@deleon.org.mx>
2017-07-09 09:02:13 -0700

    Use constant to clarify use of 26, and delete unneeded empty string check

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,6 +1,8 @@
 class Pangram
+  LETTERS_IN_ALPHABET = 26
+
   def self.pangram?(phrase)
-    !phrase.empty? && phrase.downcase.gsub(/[^a-z]/, '').chars.uniq.length == 26
+    phrase.downcase.gsub(/[^a-z]/, '').chars.uniq.length == LETTERS_IN_ALPHABET
   end
 end
 
