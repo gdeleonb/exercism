635d67fe7a34fc21a1f7e0daa938bb927038cffb
nazgus <gus@deleon.org.mx>
2017-07-12 08:57:10 -0600

    Bug fix: Take out . Define array using literal

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,12 +1,9 @@
 class Pangram
-  ALPHABET = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-  ]
+  ALPHABET = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z}
 
   def self.pangram?(phrase)
     phrase.downcase.
-           gsub(/[^[#{ALPHABET.join(',')}]]/.to_regexp, '').
+           gsub(/[^[#{ALPHABET.join(',')}]]/, '').
            chars.uniq.length == ALPHABET.length
   end
 end
