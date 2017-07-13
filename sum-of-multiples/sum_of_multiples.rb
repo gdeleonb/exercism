46622286c2cd5b153a2fc9a71d75a540783e4be1
nazgus <gus@deleon.org.mx>
2017-07-13 12:53:56 -0600

    Complete sum of multiples exercise
    Steps:
     - Establish range from 0 to one less than the limit given
     - Initialize multiples array with 0 in it (prevents errors when no multiples are found
     - For each number in it, go through the divisors that were passed into new, and add it to multiples and then break if it is divisible
     - Use reduce to add up all the multiples

---
+++ sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,23 @@
+class SumOfMultiples
+  def initialize(*args)
+    @divisors = args
+  end
+
+  def to(limit)
+    range = (0..(limit - 1))
+    multiples = [0]
+    range.each do |num|
+      @divisors.each do |divisor|
+        if num % divisor == 0
+          multiples.push(num)
+          break
+        end
+      end
+    end
+    return multiples.reduce(:+)
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
