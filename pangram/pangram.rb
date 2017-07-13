c8e53f048752809bbdc24476e15a7bef8cc353a5
nazgus <gus@deleon.org.mx>
2017-07-12 19:03:20 -0600

    Use array subtraction
     - As before, have array of desired letters
     - Create array of characters in phrase using downcase and chars
     - Subtract from alphabet array
     - Check if resulting array is empty
       - If it is, then every letter in it was used at least once, which indicates pangram

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -2,9 +2,7 @@ class Pangram
   ALPHABET = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z}
 
   def self.pangram?(phrase)
-    phrase.downcase.
-           gsub(/[^#{ALPHABET.join('')}]/, '').
-           chars.uniq.length == ALPHABET.length
+    (ALPHABET - phrase.downcase.chars).empty?
   end
 end
 
