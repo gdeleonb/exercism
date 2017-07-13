98b71df7110fe1e25da5232cba707a2fafec98f9
nazgus <gus@deleon.org.mx>
2017-07-12 18:59:47 -0600

    Implement binary exercise
    Steps:
     - Use regexp to ensure input is a valid binary number
     - Start decimal at 0
     - Turn binary into an array, reverse, and use each_with_index to run through it:
       - If digit is "1", increase decimal by 2^index

---
+++ binary/binary.rb
@@ -0,0 +1,17 @@
+class Binary
+  def self.to_decimal(binary)
+    raise ArgumentError if binary.match(/[^01]/)
+
+    decimal = 0
+    binary.chars.reverse.each_with_index do |digit, index|
+      if digit == "1"
+        decimal += 2**index
+      end
+    end
+    return decimal
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
