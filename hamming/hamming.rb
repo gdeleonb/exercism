3168ca003e214771a79d5e1f9e35500419371d46
nazgus <gus@deleon.org.mx>
2017-07-17 17:46:57 -0600

    Split long one-liner

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,6 +3,7 @@ class Hamming
 
   def self.compute(strand_1, strand_2)
     raise ArgumentError unless strand_1.length == strand_2.length
-    strand_1.chars.zip(strand_2.chars).count { |nuc_1, nuc_2| nuc_1 != nuc_2 }
+    strand_1.chars.zip(strand_2.chars).
+            count { |nuc_1, nuc_2| nuc_1 != nuc_2 }
   end
 end
