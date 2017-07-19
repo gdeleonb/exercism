1d4e371c8ca5e038d92587eb367b5964e7dea9f1
nazgus <gus@deleon.org.mx>
2017-07-18 22:12:19 -0600

    Create new version using Ruby style code
    encode(string):
     - Turn string input into char array, and chunk it based on repeats of self
      - `"aaaa" -> ['a', 'a', 'a', 'a'] -> ['a', ['a', 'a', 'a', 'a']]`
     - Map that into a new array with the number of repeats followed by the character, and flatten
      - `[['a', ['a', 'a', 'a', 'a']], ['b', ['b']] -> [[4, 'a'], [1, 'b']] -> [4, 'a', 1, 'b']`
     - Delete all 1s from the array, and join
    
    decode(string):
     - Scan for 0 or more digits, followed by any non-digit character
     - Map the repeats and the character they match with into a ternary statement that outputs the character if repeats is an empty string, or the character times repeats
     - Finally, join our array into a string

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,48 +1,15 @@
 class RunLengthEncoding
   def self.encode(input)
-    strArray = input.chars
-    len = strArray.length
-    index = 0
-    output = ''
-
-    while index < len
-      letter = strArray[index]
-      reps = countRepeats(letter, strArray[(index + 1)..-1]) + 1
-      if reps > 1
-        output << reps.to_s
-      end
-      output << letter
-      index += reps
-    end
-
-    return output
+    encoding = input.chars.chunk { |char| char }.
+      map { |char, chars| [chars.length, char] }.flatten
+    encoding.delete(1)
+    encoding.join
   end
 
-  def self.countRepeats(letter, strArray)
-    index = 0
-    while strArray[index] == letter
-      index += 1
-    end
-    return index
-  end
-
-  def self.decode(inputs)
-    strArray = inputs.chars
-    length = strArray.length
-    index = 0
-    repeats = ''
-    output = ''
-
-    while index < length
-      while strArray[index].match(/\d/)
-        repeats << strArray[index]
-        index += 1
-      end
-      output << strArray[index] * (repeats == '' ? 1 : repeats.to_i)
-      repeats = ''
-      index += 1
-    end
-    return output
+  def self.decode(input)
+    input.scan(/(\d*)(.)/).map do |repeats, char|
+      repeats == "" ? char : char * repeats.to_i
+    end.join
   end
 end
 
