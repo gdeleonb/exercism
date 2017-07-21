f7d92069463c913d4d7f2c97c759cca9bd02846f
nazgus <gus@deleon.org.mx>
2017-07-20 18:25:23 -0600

    Switch out the each for map

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -20,12 +20,14 @@ class School
   end
 
   def students_by_grade
-    output = []
     keys = @students.keys.sort
-    keys.each do |key|
-      output.push({:grade => key, :students => @students[key]}) if !@students[key].empty?
+    out = keys.map do |key|
+      if !@students[key].empty?
+        {:grade => key, :students => @students[key]}
+      end
     end
-    output
+    out.delete(nil)
+    out
   end
 end
 
