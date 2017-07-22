317a30b31998c577efd1286bf984f9229526a326
nazgus <gus@deleon.org.mx>
2017-07-22 11:11:41 -0600

    Use hash block default value to avoid having to check for nil value

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -2,16 +2,12 @@ class Phrase
   VERSION = 1
 
   def initialize(phrase)
-    @counts = {}
+    @counts = Hash.new { |hash, key| hash[key] = 0 }
     phrase.downcase.gsub(/[^a-z0-9']/, ' ').split(' ').each do |word|
-      if word[0] == "'" && word[0] == word[-1]
+      if word[0] == "'" && word[-1] == "'"
         word = word[1..-2]
       end
-      if @counts[word] == nil
-        @counts[word] = 1
-      else
-        @counts[word] += 1
-      end
+      @counts[word] += 1
     end
   end
 
