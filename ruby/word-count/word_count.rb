fd1d6a8949ae3015f0d8c402efdbb9aebd9acf69
nazgus <gus@deleon.org.mx>
2017-07-22 19:18:39 -0600

    Replace block default declaration with value
    Realized block is only needed when passing an object since the same object will be used.

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -2,7 +2,7 @@ class Phrase
   VERSION = 1
 
   def initialize(phrase)
-    @counts = Hash.new { |hash, key| hash[key] = 0 }
+    @counts = Hash.new(0)
     phrase.downcase.gsub(/[^a-z0-9']/, ' ').split(' ').each do |word|
       if word[0] == "'" && word[-1] == "'"
         word = word[1..-2]
