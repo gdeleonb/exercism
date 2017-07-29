35fcbca94b05ba90b1ffb588af97b224d48b0040
nazgus <gus@deleon.org.mx>
2017-07-29 17:19:17 -0600

    Use a much better Regexp pattern

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,17 +1,12 @@
 class Phrase
   VERSION = 1
 
-  def initialize(phrase)
-    @counts = Hash.new(0)
-    phrase.downcase.gsub(/[^a-z0-9']/, ' ').split(' ').each do |word|
-      if word[0] == "'" && word[-1] == "'"
-        word = word[1..-2]
-      end
-      @counts[word] += 1
-    end
+  def initialize string
+    @count = Hash.new(0)
+    string.downcase.scan(/\b[\w']+\b/).each { |word| @count[word] += 1 }
   end
 
   def word_count
-    @counts
+    @count
   end
 end
