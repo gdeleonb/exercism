7f77833fc293e14e8b5d5ecf5bd476dd5bf8a2f7
nazgus <gus@deleon.org.mx>
2017-07-29 17:54:48 -0600

    Store prime outside array to avoid access cost (32% speedup)

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -4,8 +4,9 @@ class Sieve
     if limit >= 2
       range = (2..limit).to_a
       until range.empty?
-        @primes << range.shift
-        range.delete_if { |num| num % @primes.last == 0 }
+        prime = range.shift
+        range.delete_if { |num| num % prime == 0 }
+        @primes << prime
       end
     end
     @primes
